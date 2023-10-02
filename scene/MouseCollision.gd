2480ebab8be9a2175c42b1f255218c3374254300
ardoardo02 <ardoardo02@yahoo.com>
2023-10-02 23:30:41 +0700

    enemies drop

--- scene/MouseCollision.gd
+++ scene/MouseCollision.gd
@@ -8,15 +8,20 @@ func _input(event):
 	if event is InputEventMouseMotion:
 		global_position = get_global_mouse_position()
 	
-	if can_drag and event is InputEventMouseButton:
+	if event is InputEventMouseButton:
 		if event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
 			var mouse_collision = $Area2D
 			for area in mouse_collision.get_overlapping_areas():
-				if area.get_parent().is_class("Item"):
+				if can_drag and area.get_parent().is_class("Item"):
 					picked_item = area.get_parent()
 					picked_item.set_drag_mode(true)
 					break
+				
+				if area.is_in_group("Loot"):
+					area.getLoot()
+					break
+					
 		
-		if event.is_released() and picked_item != null:
+		if can_drag and event.is_released() and picked_item != null:
 			picked_item.set_drag_mode(false)
 			picked_item = null
--- script/enemy_jet.gd
+++ script/enemy_jet.gd
@@ -1,8 +1,1 @@
 extends EnemyParent
-
-var direction = Vector2.RIGHT
-
-func _process(delta):
-	if health <= 0:
-		direction = Vector2.LEFT
-	translate(direction.normalized() * speed * delta)
